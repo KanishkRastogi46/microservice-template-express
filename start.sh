39f41712d12eb8ebfc18a4d9e65c630ec72494b0
Kanishk Rastogi <kanishkrastogi46@gmail.com>
2025-06-16 00:26:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-(cd api-gateway && npm install) &
-(cd auth-service && npm install) &
-(cd user-service && npm install)
+(cd api-gateway && npm install && npm run start:dev) &
+(cd auth-service && npm install && npm run start:dev) &
+(cd user-service && npm install && npm run start:dev)
+
+wait
